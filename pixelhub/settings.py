0699c2d5d482a1c7fb533e783f9cc71ebea2e7ca
Joao <joaodcarvalho@gmail.com>
2023-11-26 22:43:16 +0000

    Deploy

--- pixelhub/settings.py
+++ pixelhub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&w*-^&x6g!@)a$o+cm!-w$&o1so%rc%@e6y2ouonvxm($h=zy-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
